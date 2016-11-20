194064d6337e7fb97d6323a9e5430abb395e6a0e
setup_git_prefs <setup_git_proxy>
2016-11-21 10:08:43 +1300

    preload

--- gameFolder/js/game/states/boot.js
+++ gameFolder/js/game/states/boot.js
@@ -2,26 +2,34 @@ var vaultage = function () {};
 
 vaultage.boot= function() {};
 
-vaultage.boot.protboote = {
+vaultage.boot.prototype = {
   preload: function() {
-    this.load.image('logo', 'game/assets/images/vaultage_logo.png');
+    // images
+    this.load.image('logo', 'gameFolder/assets/images/vaultage_logo.png');
+    this.load.image('preloadBar', 'gameFolder/assets/images/load_bar.png');
+
   },
   create: function() {
+    // background colour
     this.game.stage.background = '#e6e6e6';
 
+    // no multi-touch
     this.input.maxPointers = 1;
 
+    // if on desktop
     if (this.game.device.desktop) {
       this.scale.pageAlignHorizontally = true;
+    // display mobile
     } else {
-      this.scale.scaleMode = Phaser.ScaleManager.ShowAll;
-      this.scale.minWidth = 568;
-      this.scale.minHight = 600;
-      this.scale.maxWidth = 2048;
-      this.scale.maxHight = 1536;
-      this.scale.forceLandscape = true;
-      this.scale.pageAlignHorizontally = true;
-      this.scale.setScreenSize(true);
+      this.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
+      // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      // this.scale.minWidth = 480;
+      // this.scale.minHeight = 260;
+      // this.scale.maxWidth = 1024;
+      // this.scale.maxHeight = 768;
+      // this.scale.forceLandscape = true;
+      // this.scale.pageAlignHorizontally = true;
+      // this.scale.setScreenSize(true);
     }
     this.state.start('preloader');
   }
--- gameFolder/js/main.js
+++ gameFolder/js/main.js
@@ -1,5 +1,6 @@
 var game = new Phaser.Game(820, 360, Phaser.AUTO, 'game_box');
 
 game.state.add('boot', vaultage.boot);
+game.state.add('preloader', vaultage.preload);
 
 game.state.start('boot');
