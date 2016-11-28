aa4e202f5ff943e7f1c95b0fbf0b94cae32a3ef9
setup_git_prefs <setup_git_proxy>
2016-11-28 19:32:26 +1300

    obstacles

--- gameFolder/js/game/states/game.js
+++ gameFolder/js/game/states/game.js
@@ -1,5 +1,6 @@
 vaultage.game = function() {};
-
+  this.obstacleRate = game.rnd.integerInRange(2000, 6000);
+  this.obstacleTimer = 0;
 
 vaultage.game.prototype = {
   create : function() {
@@ -9,15 +10,21 @@ vaultage.game.prototype = {
     this.game.physics.arcade.gravity.y = 1000;
 
     // sprites
+    // background
     this.background = this.game.add.tileSprite(0, 0, this.game.width, 360, 'background');
     this.background.autoScroll(-100, 0);
 
+    // ground
     this.ground = this.game.add.tileSprite(0, 290, this.game.width, 8, 'ground');
     this.ground.autoScroll(-180, 0);
 
+    // player
     this.player = this.add.sprite(45, 200, 'player');
     this.player.animations.add('run');
     this.player.animations.play('run', 15, true);
+
+    // obstacles
+    this.obstacle = this.game.add.group();
 
     // physics on sprites
     this.game.physics.arcade.enable([this.player, this.ground]);
@@ -40,10 +47,31 @@ vaultage.game.prototype = {
             this.player.body.velocity.y = -400;
         }
 
+    // randomly pick a obstacles every 2-6 seconds and place in along the ground moving towards the player
+    if (this.obstacleTimer <  this.game.time.now) {
+      this.createObstacle();
+      this.obstacleTimer = this.game.time.now + this.obstacleRate;
+    }
+
 
   },
   shutdown : function() {
 
+  }
+
+  createObstacle: function() {
+    var x = this.game.width;
+    var y = this.game.hight(290);
+
+    var obstacle = this.obstacle.getFirstExists(false);
+    if (!obstacle) {
+      obstacle = new obstacle(this.game, 0, 0);
+      this.obstacle.add(obstacle);
+    }
+    obstacle.reset(x, y);
+    obstacle.revive();
+
+    }
   }
 
 }
