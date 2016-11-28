54a6749c0346167f2df39eff063617ab2fd3a608
setup_git_prefs <setup_git_proxy>
2016-11-28 23:35:19 +1300

    updaye

--- gameFolder/js/game/states/game.js
+++ gameFolder/js/game/states/game.js
@@ -1,4 +1,7 @@
 vaultage.game = function() {};
+
+  // generate a obstacle at a random time between 2 seconds and 6 seconds
+
   this.obstacleRate = game.rnd.integerInRange(2000, 6000);
   this.obstacleTimer = 0;
 
@@ -49,6 +52,7 @@ vaultage.game.prototype = {
 
     // randomly pick a obstacles every 2-6 seconds and place in along the ground moving towards the player
     if (this.obstacleTimer <  this.game.time.now) {
+      // reset timer after each creation
       this.createObstacle();
       this.obstacleTimer = this.game.time.now + this.obstacleRate;
     }
@@ -60,14 +64,20 @@ vaultage.game.prototype = {
   }
 
   createObstacle: function() {
+    // reset box position and re-create
     var x = this.game.width;
     var y = this.game.hight(290);
 
+    // get killed box and re-use
     var obstacle = this.obstacle.getFirstExists(false);
+
+    // if no box exists create one
     if (!obstacle) {
       obstacle = new obstacle(this.game, 0, 0);
       this.obstacle.add(obstacle);
     }
+
+    // re-set box
     obstacle.reset(x, y);
     obstacle.revive();
 
